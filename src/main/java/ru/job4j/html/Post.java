f78d239ea06a767f443d00de7982dca31944d2a4
Constantine Burovytsky <esfero1505@gmail.com>
2020-06-08 22:16:36 +0300

    2.2. Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/html/Post.java
@@ -0,0 +1,60 @@
+package ru.job4j.html;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Post {
+    private int id;
+    private String name;
+    private String description;
+    private LocalDateTime created;
+
+    public Post(int id, String name, String description, LocalDateTime created) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return id == post.id &&
+                Objects.equals(name, post.name) &&
+                Objects.equals(description, post.description) &&
+                Objects.equals(created, post.created);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, created);
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", created=" + created +
+                '}';
+    }
+}
